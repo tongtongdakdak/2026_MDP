3267cfe5ac7e773acb8dccdbb663e02c5c05c24e
tongtongdakdak <namulhr08@naver.com>
2026-09-14 16:14:14 +0900

    last_update

--- model.py
+++ model.py
@@ -1,23 +1,31 @@
 import os
 import json
 from ultralytics import YOLO
+from peft import LoraConfig, get_peft_model
 
 DRIVE_SAVE_PATH = r'C:\Users\namul\Desktop\2026_MDP'
-DATASET_ROOT = os.path.join(DRIVE_SAVE_PATH, 'dataset')
-NEW_DATA_ROOT = os.path.join(DATASET_ROOT, 'new_data.yaml')
-DATA_YAML_PATH = os.path.join(DATASET_ROOT, 'data.yaml')
 
 def train_yolo():
-    last_checkpoint = os.path.join(DRIVE_SAVE_PATH, 'lego_detection_5/weights/last.pt')
+    model = YOLO('yolo26n.pt') 
 
-    if os.path.exists(last_checkpoint):
-        model = YOLO(last_checkpoint)
-        model.train(resume=True)
-    else:
-        model = YOLO('yolo26n.pt') 
-        
-        config_path = os.path.join(DRIVE_SAVE_PATH, 'config.json')
-        with open(config_path, 'r', encoding='utf-8') as f:
-            train_config = json.load(f)
-        
-        model.train(**train_config)
+    base_model = model.model
+    for param in base_model.parameters():
+        param.requires_grad = False
+
+    peft_config = LoraConfig(
+        r=8,
+        lora_alpha=16,
+        target_modules=["cv1", "cv2"],
+        lora_dropout=0.05,
+        bias="none"
+    )
+
+    model.model = get_peft_model(base_model, peft_config)
+    
+    model.model.print_trainable_parameters()
+
+    config_path = os.path.join(DRIVE_SAVE_PATH, 'config.json')
+    with open(config_path, 'r', encoding='utf-8') as f:
+        train_config = json.load(f)
+
+    model.train(**train_config)
